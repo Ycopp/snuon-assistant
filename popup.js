9e46b3e893bed11cf3c840a78376bf8416a342ae
ylemkimon <mail@ylem.kim>
2019-04-07 10:53:25 +0900

    Remove browser action

--- popup.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('#config').addEventListener('click', () => {
-    window.open(chrome.runtime.getURL('options.html'));
-  });
-
-  document.querySelector('#about').addEventListener('click', () => {
-    window.open('https://www.facebook.com/permalink.php?story_fbid=127490438357751&id=100032903859782');
-  });
-});
